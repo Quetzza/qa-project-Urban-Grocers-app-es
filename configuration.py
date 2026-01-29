5a18a61ff795b4f274cf85b721d993ec66fee32b
Axel Arteaga <axelarteaga21@gmail.com>
2026-01-29 02:34:27 -0600

    <refactor>[configuration.py]: Last reset URL

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-f401d7f2-7fb1-4572-942f-23030cd7de9c.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-438b4331-5d70-4e65-95ee-d1a4402f8ef1.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 
 CREATE_USER_PATH = "/api/v1/users"# Almacena la ruta para crear un usuario o usuaria en esta variable
 
