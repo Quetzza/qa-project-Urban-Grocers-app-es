07a908ca86b2d48768f37e4912a417e3da05f3f1
Axel Arteaga <axelarteaga21@gmail.com>
2026-01-28 23:53:23 -0600

    <add>[negative_test.py]: Funtion negative assert no key name

--- negative_test.py
+++ negative_test.py
@@ -48,3 +48,27 @@ def negative_assert_characters_is_greater_than_the_upper_limit(name):
     # assert kit_response.json()["code"] == 400
 
     # assert kit_response.json()["message"] == "El nombre debe contener sólo letras latino, un espacio y un guión. De 2 a 15 caracteres"
+
+def negative_assert_no_key_name(name):
+
+    user_body = getters.get_user_body()
+
+    headers_body = getters.get_headers()
+
+    user_response = sender_stand_request.post_new_user(user_body, headers_body)
+
+    assert user_response.status_code == 201 # Created
+
+    assert user_response.json()["authToken"] != "" # False
+
+    kit_body = getters.get_kit_body(name)
+
+    headers_authToken_body = getters.get_headers_authToken(user_response.json()["authToken"])
+
+    kit_response = sender_stand_request.post_new_kit(kit_body, headers_authToken_body)
+
+    assert kit_response.status_code == 400, f"Se esperaba code: 400"
+
+    # assert kit_response.json()["code"] == 400
+
+    # assert kit_response.json()["message"] == "El nombre debe contener sólo letras latino, un espacio y un guión. De 2 a 15 caracteres"
